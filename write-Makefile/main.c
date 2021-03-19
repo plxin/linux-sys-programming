f01ed142cef02c2f46e3d3513607c0c602e1bc00
plxin <2598935264@qq.com>
2021-03-19 15:28:09 +0800

    add some files about poll

--- write-Makefile/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <public.h>
-int main()
-{
-    add(1, 2);
-    sub(5, 1);
-
-    return 0;
-}
--- write-Makefile/test1/include/common.h
+++
@@ -1,9 +0,0 @@
-#ifdef COMMON_H_
-#define COMMON_H_
-
-int add(int a, int b);
-int sub(int a, int b);
-int div(int a, int b);
-int mul(int a, int b);
-
-#endif
